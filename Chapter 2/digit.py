16cd06dcd65be9573d32d253c26ee5831a781b24
Rohit Singh <rohitsinghsomvanci00@gmail.com>
2026-06-05 18:12:52 +0530

    Added student Analysis Project

--- Chapter 2/digit.py
+++ Chapter 2/digit.py
@@ -1,11 +1,11 @@
-# def digit(n):
-#     while(n != 0):
-#        d=n%10
-#        print(d)
-#        n = n//10   # '/'  python me ye divide karke point value deta hai "//" perfect value deta hai aur quotient return karta hai .
-# # print(n)   
-# n=int(input("Enter a num:")) 
-# digit(n)             
+def digit(n):
+    while(n != 0):
+       d=n%10   # "%" ye mod hai jo ki percent bhi hai ye divide karne par remender deta hai. 
+       print(d)
+       n = n//10   # '/'  python me ye divide karke point value deta hai "//" perfect value deta hai aur quotient return karta hai .
+    #print(n)   
+n=int(input("Enter a num:")) 
+digit(n)             
     
     
 
